b3d524142d1d2429ab294c57531ad551879b29ea
C2595918590 <2595918590@qq.com>
2022-09-11 16:06:42 +0800

    第一次上传

---
+++ data/home/binding/css js/url.js
@@ -0,0 +1,11 @@
+//接口
+
+//用户信息
+const users = 'https://jsonplaceholder.typicode.com/users';
+
+//图片
+const photos = 'https://jsonplaceholder.typicode.com/photos';
+
+//文章
+const posts = 'https://jsonplaceholder.typicode.com/posts';
+export default {users,photos,posts}
